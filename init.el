36d2801f39ebfdd1a1ce6a529e4876bdd635eff2
hiroseatsushi2600 <hiroseatsushi2600@gmail.com>
2020-12-06 18:05:09 +0900

    test

--- init.el
+++ init.el
@@ -1,8 +1,17 @@
 (add-to-list 'load-path "~/.emacs.d/conf")
 
-(load "package.el")
+(load "init-package.el")
 (load "basic.el")
 (load "billgates.el")
+
+;; ‚Æ‚è‚ ‚¦‚¸
+(add-to-list 'load-path "~/.emacs.d/vendor/twittering-mode-3.0.0")
+(require 'twittering-mode)
+(setq twittering-use-master-password t)
+;(setq twittering-allow-insecure-server-cert t)
+
+;(setq twittering-curl-program (concat site-lisp-path "~/.emacs.d/vendor/twittering-mode-3.0.0/win-curl/curl.exe"))
+;(setq twittering-cert-file (concat site-lisp-path "~/.emacs.d/vendor/twittering-mode-3.0.0/win-curl/equifax.cer"))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -18,5 +27,6 @@
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
  '(cua-mode t nil (cua-base))
+ '(package-selected-packages '(anzu atom-one-dark-theme undo-tree bind-key which-key))
  '(show-paren-mode t)
  '(tool-bar-mode nil))
